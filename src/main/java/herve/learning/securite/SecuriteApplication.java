b02453a03f343d3aac194fcc8e68d3708156b341
Ekem herve <ekemherve@gmail.com>
2018-11-18 20:51:44 +0100

    initialise ingredient  database

--- src/main/java/herve/learning/securite/SecuriteApplication.java
+++ src/main/java/herve/learning/securite/SecuriteApplication.java
@@ -1,5 +1,6 @@
 package herve.learning.securite;
 
+import herve.learning.securite.dataAccess.entity.IngredientEntity;
 import herve.learning.securite.dataAccess.entity.RoleEntity;
 import herve.learning.securite.dataAccess.repository.RoleRepository;
 import herve.learning.securite.model.RoleEnum;
@@ -32,6 +33,80 @@ public class SecuriteApplication {
 
             roleRepository.save(user);
             roleRepository.save(admin);
+
+            IngredientEntity cheddar = new IngredientEntity();
+            cheddar.setName("Fromage cheddar");
+            cheddar.setStock(200);
+
+            IngredientEntity emmental = new IngredientEntity();
+            emmental.setName("Fromage emmental");
+            emmental.setStock(200);
+
+            IngredientEntity goudin = new IngredientEntity();
+            goudin.setName("Fromage goudin");
+            goudin.setStock(200);
+
+            IngredientEntity cammenber = new IngredientEntity();
+            cammenber.setName("Fromage cammenber");
+            cammenber.setStock(200);
+
+            IngredientEntity tomate = new IngredientEntity();
+            tomate.setName("tomate");
+            tomate.setStock(200);
+
+            IngredientEntity celeri = new IngredientEntity();
+            celeri.setName("celeri");
+            celeri.setStock(200);
+
+            IngredientEntity poivron = new IngredientEntity();
+            poivron.setName("poivron");
+            poivron.setStock(200);
+
+            IngredientEntity oignon = new IngredientEntity();
+            oignon.setName("oignon");
+            oignon.setStock(200);
+
+            IngredientEntity ail = new IngredientEntity();
+            ail.setName("ail");
+            ail.setStock(200);
+
+            IngredientEntity champignon = new IngredientEntity();
+            champignon.setName("champignon");
+            champignon.setStock(200);
+
+            IngredientEntity olive = new IngredientEntity();
+            olive.setName("olive");
+            olive.setStock(200);
+
+            IngredientEntity scampi = new IngredientEntity();
+            scampi.setName("scampi");
+            scampi.setStock(200);
+
+            IngredientEntity poulet = new IngredientEntity();
+            poulet.setName("poulet");
+            poulet.setStock(200);
+
+            IngredientEntity viande = new IngredientEntity();
+            viande.setName("viande");
+            viande.setStock(200);
+
+            IngredientEntity kefta = new IngredientEntity();
+            kefta.setName("kefta");
+            kefta.setStock(200);
+
+            IngredientEntity jambon = new IngredientEntity();
+            jambon.setName("jambon");
+            jambon.setStock(200);
+
+            IngredientEntity thon = new IngredientEntity();
+            thon.setName("thon");
+            thon.setStock(200);
+
+            IngredientEntity pepperoni = new IngredientEntity();
+            pepperoni.setName("pepperoni");
+            pepperoni.setStock(200);
+
+
         };
     }*/
 }
